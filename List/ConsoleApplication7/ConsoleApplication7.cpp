0f991781cf340cd940286acf96ee4e14c762e3c8
Vyacheslav <tagart.ru@mail.ru>
2018-06-10 14:11:31 +0600

    finished list

---
+++ List/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,125 @@
+// ConsoleApplication7.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "L2ListImplementation.h"
+using namespace std;
+
+void drawMenu()
+{
+    cout << "1.Exit" << endl;
+    cout << "2.Push" << endl;
+    cout << "3.Remove" << endl;
+    cout << "4.Show" << endl;
+    cout << "5.Next" << endl;
+    cout << "6.Start Iterator" << endl;
+    cout << "7.is Finish" << endl;
+    cout << "8.Clear" << endl;
+    cout << "9.No iterator push" << endl;
+    cout << "10.Print list" << endl;
+}
+
+int main()
+{
+    L2Ring<int> *list = new L2Ring<int>();
+    Iterator<int>* iter = nullptr;
+    int a = 0;
+    bool ext = false;
+    while(!ext)
+    {
+        drawMenu();
+        cin >> a;
+        system("cls");
+        switch(a)
+        {
+        case 1:ext = true; break;
+        case 2:try
+        {
+            cout << "Value to add: "; cin >> a; list->push(a, iter);
+        }
+               catch(runtime_error e)
+               {
+                   cerr << e.what() << endl;
+               }break;
+        case 3:try
+        {
+            list->remove(iter);
+        }
+               catch(runtime_error e)
+               {
+                   cerr << e.what() << endl;
+               }break;
+        case 4:try
+        {
+            cout << "Current value: " << iter->get() << endl; 
+        }
+               catch(runtime_error e)
+               {
+                   cerr << e.what() << endl;
+               }break;
+        case 5:try
+        {
+            iter->next();
+        }
+               catch(std::runtime_error e)
+               {
+                   cerr << e.what() << endl;
+               }break;
+        case 6:try
+        {
+            if(iter == nullptr)iter = list->iterator();
+            iter->start();
+        }
+               catch(std::runtime_error e)
+               {
+                   cerr << e.what() << endl;
+               }break;
+        case 7:cout << iter->finish()<<endl; break;
+        case 8:list->clear(); break;
+        case 9:cout << "Value to add: "; cin >> a; list->push(a); break;
+        case 10:
+        {
+            Iterator<int> *it = list->iterator();
+            it->start();
+            while(!it->finish())
+            {
+                cout << it->get() << endl;
+                it->next();
+            }
+        }
+        default:cout << "List size: " << list->length() << endl;
+        }
+    }
+    list->push(1);
+    list->push(2);
+    list->push(3);
+    list->clear();
+    list->push(228);
+    list->push(314159);
+    list->push(5);
+    iter = list->iterator();
+    iter->start();
+    while(!iter->finish())
+    {
+        cout << iter->get() << endl;
+        iter->next();
+    }
+    L2Ring<string>& list1 = L2Ring<string>();
+    list1.push("First");
+    list1.push("Second");
+    list1.push("Third");
+    L2Ring<string> list2 = list1;
+    Iterator<string> *iter2 = list2.iterator();
+    string str = "";
+    iter2->start();
+    while(!iter2->finish())
+    {
+        str = iter2->get();
+        cout << str << endl;
+        iter2->next();
+    }
+    system("pause");
+    return 0;
+}
---
+++ List/ConsoleApplication7/Iterator.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "L2ListImplementation.h"
+
+template <typename T>
+class L2List;
+
+template <typename T>
+class L2Ring;
+
+template <typename T>
+class Iterator;
+
+template <typename T>
+class L2Iterator;
+
+template <typename T>
+struct Node
+{
+    friend Iterator<T>;
+    friend L2Iterator<T>;
+    friend L2Ring<T>;
+    T value;
+    Node *next, *prev;
+    Node(T value, Node *next, Node *prev)
+    {
+        this->value = value;
+        this->next = next;
+        this->prev = prev;
+    }
+
+    Node(Node *next, Node *prev)
+    {
+        this->next = next;
+        this->prev = prev;
+    }
+};
+
+template <typename T>
+class Iterator
+{
+    Node<T> *current = nullptr;
+    L2List<T> *list = nullptr;
+public:
+    friend L2Ring<T>;
+    friend L2Iterator<T>;
+    virtual void start() = 0;
+    virtual T get() = 0;
+    virtual void next() = 0;
+    virtual bool finish() = 0;
+};
